17027acfbed0a0401684e284b77fe7a562654d36
Carlos Chacin <cchacin@gmail.com>
2015-05-05 19:55:50 -0700

    Refactor

--- src/main/java/com/github/cchacin/cucumber/steps/RestSteps.java
+++ src/main/java/com/github/cchacin/cucumber/steps/RestSteps.java
@@ -20,15 +20,9 @@ import cucumber.api.DataTable;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
+import static com.github.cchacin.cucumber.steps.Utility.fileContent;
 import static com.jayway.restassured.RestAssured.given;
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,20 +35,6 @@ public class RestSteps {
     private String responseValue;
     private String basePath;
 
-    final String fileContent(final String filePath) throws URISyntaxException, IOException {
-
-        final URL url = Thread.currentThread().getContextClassLoader().getResource("./"+filePath);
-        if(null == url){
-            throw new IOException(String.format("(%s) File not found in path (%s)",
-                                                filePath,
-                                                Thread.currentThread().getContextClassLoader().getResource("./")));
-        }
-
-        final byte[] encoded = Files.readAllBytes(Paths.get(url.toURI()));
-
-        return StandardCharsets.UTF_8.decode(ByteBuffer.wrap(encoded))
-                .toString();
-    }
 
     final RequestSpecification createWebClient(final String endpointUrl) {
         this.basePath = endpointUrl;
